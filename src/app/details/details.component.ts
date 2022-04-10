189282ba76912256ab2f2391d5d8123773040e5d
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-10 21:49:35 +0300

    adds form to the app.component.html

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NewQuote } from '../new-quote';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
-  quotes: NewQuote []= [
-    new NewQuote (1, 'this one','sue',0,0),
-    new NewQuote (2, 'this two', 'vuvu',0,0),
-    new NewQuote (3, 'this three','deno',0,0)
-  ]
+  quotes: NewQuote[] = [
+    new NewQuote(1, 'this one', 'sue', 0, 0),
+    // new NewQuote1(2, 'this two', 'vuvu', 0, 0),
+    // new NewQuote2(3, 'this three', 'deno', 0, 0),
+  ];
+  @Input()
+  author!: NewQuote;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
