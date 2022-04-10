000160a235789d616abd927a5ba2777acde28bfd
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-11 00:17:49 +0300

    generates the quote-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuotesComponent } from './quotes/quotes.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,3 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Quotes';
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { HighlightDirective } from './highlight.directive';
 import { DetailsComponent } from './details/details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     HighlightDirective,
-    DetailsComponent
+    DetailsComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
